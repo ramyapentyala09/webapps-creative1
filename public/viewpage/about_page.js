028aca5b92ae6c66e3b86acb2184d7f72a96125f
ramyapentyala <ramyapentyala@gmail.com>
2022-02-04 22:39:51 -0600

    final

--- public/viewpage/about_page.js
+++ public/viewpage/about_page.js
@@ -16,6 +16,14 @@ export function about_page(){
         return;
     }
     Elements.root.innerHTML = `
-    <h1>About Page</h1>
+    <div class="text-center">
+              <img
+                class="rounded float"
+                src="https://t4.ftcdn.net/jpg/00/67/67/67/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
+                alt="image"
+                style="width: 200px; height: 200px;"
+              />
+            </div>
+    <h3 class ="text-center">Logged in as: ${currentUser.email} </h3>
     `;
 }
