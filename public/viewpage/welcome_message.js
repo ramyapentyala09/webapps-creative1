b3f28aaa87891c7d204fd1abac8d530b1fe4a9e8
ramyapentyala <ramyapentyala@gmail.com>
2022-02-04 21:24:16 -0600

    final

--- public/viewpage/welcome_message.js
+++ public/viewpage/welcome_message.js
@@ -2,7 +2,7 @@ export const html = `
 <h1> Welcome to Awesome Discussion Forum</h1>
 Please sign in to do:
 <ul>
-    <li> To Create a thread</li>
+    <li> To Create, Update, Delete a thread</li>
     <li> To browse thread</li>
     <li> To reply messages to thread</li>
 </ul>
